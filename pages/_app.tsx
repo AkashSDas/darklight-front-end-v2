8ff74770dfc3cf97f8fc7fcdb744384f67ccd55c
AkashSDas <aakashdas368@gmail.com>
2022-09-15 23:11:01 +0530

    use toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "@styles/globals.css";
 import { NextPage } from "next";
 import { AppProps } from "next/app";
 import { ReactElement, ReactNode } from "react";
+import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 
 import store from "@store/index";
@@ -20,7 +21,10 @@ const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+    <Provider store={store}>
+      <Toaster position="top-center" reverseOrder={false} />
+      {getLayout(<Component {...pageProps} />)}
+    </Provider>
   );
 };
 
