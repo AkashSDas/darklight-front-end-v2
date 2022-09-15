a338bd9b3bb96dbaed0207ada6b4bf8e49a3b608
AkashSDas <aakashdas368@gmail.com>
2022-09-15 14:29:04 +0530

    use per page layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,26 @@
 import "@styles/globals.css";
 
+import { NextPage } from "next";
 import { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
 import { Provider } from "react-redux";
 
 import store from "@store/index";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
   );
 };
 
