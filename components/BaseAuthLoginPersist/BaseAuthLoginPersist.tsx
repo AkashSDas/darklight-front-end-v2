46e6d6bf24bd10541eafbaf53e255df963a6f55a
AkashSDas <aakashdas368@gmail.com>
2022-09-16 11:14:44 +0530

    refactor

--- components/BaseAuthLoginPersist/BaseAuthLoginPersist.tsx
+++ components/BaseAuthLoginPersist/BaseAuthLoginPersist.tsx
@@ -1,53 +1,16 @@
 import Link from "next/link";
-import { PropsWithChildren, useEffect, useRef, useState } from "react";
+import { PropsWithChildren } from "react";
 
-import { useAppDispatch, useAppSelector } from "@hooks/store";
-import { selectToken } from "@store/base-auth/slice";
-import { selectInitLoading } from "@store/login/slice";
-import { refreshThunk } from "@store/login/thunk";
+import { useTokenRefresh } from "@hooks/auth";
 
 export const BaseAuthLoginPersist = ({ children }: PropsWithChildren<{}>) => {
-  // To make sure that `token` i.e. access token has been set in the store
-  // https://youtu.be/9YnZHQsWmJs?list=PL0Zuz27SZ-6P4dQUsoDatjEGpmBpcOW8V&t=1891
-  const [success, setSuccess] = useState(true);
-
-  const token = useAppSelector(selectToken);
-  const initLoading = useAppSelector(selectInitLoading);
-  const dispatch = useAppDispatch();
-
-  /** To check if the useEffect already ran OR not */
-  const effectRan = useRef(false);
-
-  // This should run only once
-  useEffect(() => {
-    // To avoid issue with React 18 strict mode which mounts, unmounts and mounts again
-    // https://youtu.be/9YnZHQsWmJs?list=PL0Zuz27SZ-6P4dQUsoDatjEGpmBpcOW8V&t=1807
-    if (effectRan.current === true || process.env.NODE_ENV !== "development") {
-      const refreshAccessToken = async () => {
-        console.log("Refreshing access token");
-        const response = await (await dispatch(refreshThunk())).payload;
-        if (response === true) {
-          setSuccess(true);
-        } else {
-          setSuccess(false);
-        }
-      };
-
-      // When you refresh the page, the token in the store is null and to set it
-      // we've to run refresh access token
-      if (!token) refreshAccessToken();
-    }
-
-    return () => {
-      effectRan.current = true;
-    };
-  }, []);
+  const { initLoading, isAccessTokenSet, accessToken } = useTokenRefresh();
 
   if (initLoading) return <div>Loading...</div>;
-  if ((!initLoading && !success) || !token)
+  if ((!initLoading && !isAccessTokenSet) || !accessToken)
     return (
-      <div>
-        <h1>Session expired. Please login</h1>
+      <div className="p-4 rounded-lg border border-clay">
+        <h5>Session expired. Please login</h5>
         <Link href="/auth/login">Login</Link>
       </div>
     );
---
+++ hooks/auth.ts
@@ -0,0 +1,50 @@
+import { useEffect, useRef, useState } from "react";
+
+import { selectToken } from "@store/base-auth/slice";
+import { selectInitLoading } from "@store/login/slice";
+import { refreshThunk } from "@store/login/thunk";
+
+import { useAppDispatch, useAppSelector } from "./store";
+
+/**
+ * This hook will run only once i.e. during the page's initial load
+ */
+export const useTokenRefresh = () => {
+  const accessToken = useAppSelector(selectToken);
+  const initLoading = useAppSelector(selectInitLoading);
+
+  // To make sure that `token` i.e. access token has been set in the store
+  // https://youtu.be/9YnZHQsWmJs?list=PL0Zuz27SZ-6P4dQUsoDatjEGpmBpcOW8V&t=1891
+  const [isAccessTokenSet, setIsAccessTokenSet] = useState(true);
+
+  /** To check if the useEffect already ran OR not */
+  const effectRan = useRef(false);
+
+  const dispatch = useAppDispatch();
+
+  // This should run only once
+  useEffect(() => {
+    // To avoid issue with React 18 strict mode which mounts, unmounts and mounts again
+    // https://youtu.be/9YnZHQsWmJs?list=PL0Zuz27SZ-6P4dQUsoDatjEGpmBpcOW8V&t=1807
+    if (effectRan.current === true || process.env.NODE_ENV !== "development") {
+      const refreshAccessToken = async () => {
+        const response = await (await dispatch(refreshThunk())).payload;
+        if (response === true) {
+          setIsAccessTokenSet(true);
+        } else {
+          setIsAccessTokenSet(false);
+        }
+      };
+
+      // When you refresh the page, the token in the store is null and to set it
+      // we've to run refresh access token
+      if (!accessToken) refreshAccessToken();
+    }
+
+    return () => {
+      effectRan.current = true;
+    };
+  }, []);
+
+  return { isAccessTokenSet, initLoading, accessToken };
+};
